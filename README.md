ea7a053c4530076bf19607048fc89548ee57f699
Hemant Bhoi <56757949+hkbcode@users.noreply.github.com>
2021-04-22 12:04:35 +0530

    chakraui added

--- README.md
+++ README.md
@@ -1,10 +1,1 @@
-## Reactjs app starter with Material UI
-
-
-Start Building React apps with
-
-:point_down:
-
-- React
-- Typescript
-- MaterialUI
+## Reactjs app with chakraui
--- src/App.tsx
+++ src/App.tsx
@@ -1,22 +1,38 @@
-import React from 'react';
-import Header from './components/Header';
-import Button from '@material-ui/core/Button';
-import Typography from '@material-ui/core/Typography';
+import * as React from "react"
+import {
+  ChakraProvider,
+  Box,
+  Text,
+  Link,
+  VStack,
+  Code,
+  Grid,
+  theme,
+} from "@chakra-ui/react"
+import { ColorModeSwitcher } from "./ColorModeSwitcher"
+import { Logo } from "./Logo"
 
-
-
-
-const App = () =>{
-  return(
-  <div> 
-    <Typography variant="h2" style={{textAlign:"center"}}>Reactjs with MaterialUI</Typography>
-    <Button variant="contained" color="primary"  style={{marginLeft:650}}>Now Start Coding</Button>
-    <Header />
-  </div>
-
-  )
-}
-
-
-
-export default App
+export const App = () => (
+  <ChakraProvider theme={theme}>
+    <Box textAlign="center" fontSize="xl">
+      <Grid minH="100vh" p={3}>
+        <ColorModeSwitcher justifySelf="flex-end" />
+        <VStack spacing={8}>
+          <Logo h="40vmin" pointerEvents="none" />
+          <Text>
+            Edit <Code fontSize="xl">src/App.tsx</Code> and save to reload.
+          </Text>
+          <Link
+            color="teal.500"
+            href="https://chakra-ui.com"
+            fontSize="2xl"
+            target="_blank"
+            rel="noopener noreferrer"
+          >
+            Learn Chakra
+          </Link>
+        </VStack>
+      </Grid>
+    </Box>
+  </ChakraProvider>
+)
